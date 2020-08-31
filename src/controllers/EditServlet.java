ff097bd1e3386167f4f895659407c4b6776e9679
masaro.matsumoto <m.masaro.h8@gmail.com>
2020-08-31 21:48:06 +0900

    Add getAllTask

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,8 +1,6 @@
-
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -16,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,34 +33,42 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-//      response.getWriter().append("Served at: ").append(request.getContextPath());
+        //response.getWriter().append("Served at: ").append(request.getContextPath());
 
+
+
+
+
+        // 編集するデータを取得
         EntityManager em = DBUtil.createEntityManager();
-
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
-                                   .getResultList();
-
-//        response.getWriter().append(Integer.valueOf(messages.size()).toString());
-
-
-        request.setAttribute("tasks",tasks);
-
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
         em.close();
 
+        request.setAttribute("task", t);
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+
+
+        //task idをセッションに保存
+        if(t != null) {
+            request.getSession().setAttribute("task_id", t.getId());
+        }
+
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
 
-    }
 
+    }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    /*
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         doGet(request, response);
     }
-*/
+
 }
--- src/models/Task.java
+++ src/models/Task.java
@@ -1,3 +1,4 @@
+
 package models;
 
 import java.sql.Timestamp;
@@ -12,21 +13,25 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+
 @NamedQueries({
     @NamedQuery(
-            name = "getAllMessages",
-            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            name = "getAllTasks",
+            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
             )
 })
-@Table(name = "messages")
-public class Message {
+
+@Table(name = "tasks")
+
+
+
+
+public class Task {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -43,14 +48,6 @@ public class Message {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
@@ -76,4 +73,5 @@ public class Message {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
+
 }
