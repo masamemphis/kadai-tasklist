ac5c28c6c73f5be9b3a2fb93cb8491e0da9dc544
masaro.matsumoto <m.masaro.h8@gmail.com>
2020-09-02 17:16:34 +0900

    Modify UpdatateServlet

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import models.TaskValidator.TaskValidator;
+import models.TaskValidators.TaskValidator;
 import utils.DBUtil;
 
 /**
--- src/models/TaskValidators/TaskValidator.java
+++ src/models/TaskValidators/TaskValidator.java
@@ -1,4 +1,4 @@
-package models.TaskValidator;
+package models.TaskValidators;
 
 
 
